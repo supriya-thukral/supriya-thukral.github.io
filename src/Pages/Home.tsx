f8ab0a523f30bb2e7a6da67ac998c31527c46488
sthukral <supriyathukral@hotmail.com>
2023-06-16 22:35:55 -0400

    update hero-row

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedin, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import '../Images/SupriyaThukral.jpg'
 
 const HomePage: React.FC = () => {
     return (
         <div>
-            <section className="container rounded shadow-lg" id="about-box">
-                <div className="row">
-                    <div className="col-sm-6 mx-auto">
+            <section className="container rounded" id="about-box">
+                <div className="row hero-row">
+                    <div className="col-sm-3">
                         <figure>
-                            <img className="img-fluid align-self-center shadow-lg rounded" src="SupriyaThukral.jpg" alt="Photo of Supriya Thukral" />
+                            <img src={require('../Images/SupriyaThukral.jpg')} className="img-fluid rounded" alt="Supriya Thukral" />
                         </figure>
                     </div>
-                    <div className="col-sm-6 text-center pt-5">
+                    <div className="col-sm-6 text-center">
                         <div className="container">
                             <h1 id="intro">Hi! I'm Supriya!</h1>
                             <p>I am a Software Engineering Intern at Forma.AI, where I am gaining valuable experience in developing cutting-edge solutions.</p>
@@ -22,13 +23,13 @@ const HomePage: React.FC = () => {
                                 <p>Connect with me:</p>
                                 <p>
                                     <a href="https://www.linkedin.com/in/supriyathukral/" target="_blank" rel="noopener noreferrer">
-                                        <FontAwesomeIcon icon={faLinkedin} className='fa-icon' />
+                                        <FontAwesomeIcon icon={faLinkedin} className="fa-icon" />
                                     </a>
-                                    <a href="https://github.com/sthukral" target="_blank" rel="noopener noreferrer" className='fa-icon'>
+                                    <a href="https://github.com/sthukral" target="_blank" rel="noopener noreferrer" className="fa-icon">
                                         <FontAwesomeIcon icon={faGithub} />
                                     </a>
                                     <a href="https://www.instagram.com/chasingmomentsphotoco/" target="_blank" rel="noopener noreferrer">
-                                        <FontAwesomeIcon icon={faInstagram} className='fa-icon' />
+                                        <FontAwesomeIcon icon={faInstagram} className="fa-icon" />
                                     </a>
                                 </p>
                                 <a className="btn btn-primary" id="devpost" href="https://devpost.com/software/covwe" role="button" target="_blank" rel="noopener noreferrer">Latest Hackathon »</a>

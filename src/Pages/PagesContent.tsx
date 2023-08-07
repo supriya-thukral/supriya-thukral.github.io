23152d3e25a73793a7856349864fa8dcf3f24c3e
sthukral <supriyathukral@hotmail.com>
2023-08-07 15:03:52 -0400

    add education section

--- src/Pages/PagesContent.tsx
+++ src/Pages/PagesContent.tsx
@@ -1,4 +1,5 @@
 import ContactPage from "./ContactPage";
+import EducationPage from "./EducationPage";
 import ExperiencePage from "./ExperiencePage";
 import HomePage from "./HomePage";
 import ProjectsPage from "./ProjectsPage";
@@ -30,6 +31,11 @@ export const pagesContent: {
     title: "Projects",
     href: "projects",
     component: ProjectsPage,
+  },
+  education: {
+    title: "Education",
+    href: "education",
+    component: EducationPage,
   },
   contact: {
     title: "Contact",
